7479424584162a69896e3d937a973f7ffb81497c
joshtanguay <joshtanguay@gmail.com>
2020-05-07 11:52:58 -0700

    added shop and collections

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 import "./App.css";
-
-const SculpturesPage = () => (
-  <div className="sculptures-page">
-    <h1>Sculptures</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/sculptures" component={SculpturesPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );

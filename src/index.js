0ddb2c08a66da2ea742ec83d307342b2d7cc333b
joshtanguay <joshtanguay@gmail.com>
2020-05-15 11:21:13 -0700

    increased size of logo and navigation text

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
-import store from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 
 import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={process.env.PUBLIC_URL || "lofrano-arts"}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")

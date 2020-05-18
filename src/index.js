9024764d7f897a43aedc784f365919ebeb251e3e
joshtanguay <joshtanguay@gmail.com>
2020-05-18 10:51:01 -0700

    created component to handle scroll restoration to scroll page to the top with all navigation within site

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ScrollToTop from "./components/scroll-to-top/scroll-to-top.component.jsx";
 import App from "./App";
 import { store, persistor } from "./redux/store.js";
 
@@ -12,6 +13,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={process.env.PUBLIC_URL || "lofrano-arts"}>
       <PersistGate persistor={persistor}>
+        <ScrollToTop />
         <App />
       </PersistGate>
     </BrowserRouter>

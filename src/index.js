ec599ae45171206f100b983855ec14ebac7df6b5
joshtanguay <joshtanguay@gmail.com>
2020-05-18 13:51:45 -0700

    updated browswer router basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL || "lofrano-arts"}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <PersistGate persistor={persistor}>
         <ScrollToTop />
         <App />

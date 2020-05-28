556debd3fb5c4c99739ee6a894bfd7c103393951
joshtanguay <joshtanguay@gmail.com>
2020-05-28 13:13:43 -0700

    updated footer placeholder

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import instagramLogo from "../../assets/Instagram_logo.png";
 
 import "./footer.styles.scss";
 
 const Footer = () => (
   <div className="footer">
-    <h2 className="footer-text">This is our footer</h2>
+    <div className="footer-left footer-text">
+      <a href="https://www.instagram.com/jloafrano/" target="blank">
+        <img
+          className="social-media-logo"
+          src={instagramLogo}
+          alt="instagram-account"
+        ></img>
+      </a>
+    </div>
+    <div className="footer-right footer-text">
+      <p>Terms and Privacy</p>
+      <p>Contact</p>
+    </div>
+    <div className="footer-bottom footer-text">
+      <p>Copyright © Lofrano Arts 2020</p>
+    </div>
   </div>
 );
 

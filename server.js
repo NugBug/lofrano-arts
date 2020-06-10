8ef01e67a1584b4ae548c5e1ae7a60ec43b18c7e
joshtanguay <joshtanguay@gmail.com>
2020-06-10 12:20:09 -0700

    updating our code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log("Server is running on port " + port);
+});
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, ".", "build", "service-worker.js"));
 });
 
 app.post("/payment", function (req, res) {
@@ -45,8 +49,4 @@ app.post("/payment", function (req, res) {
       res.status(200).send({ success: stripeResponse });
     }
   });
-});
-
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });

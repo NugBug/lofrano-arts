2518319b86ff0882d881421ca42343a727f5ddf2
joshtanguay <joshtanguay@gmail.com>
2020-06-10 13:12:01 -0700

    updating our code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const compression = require("compression");
 const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
@@ -14,16 +13,11 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("/testing", (req, res) => {
-    res.send(console.log("this is working"));
-  });
 
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
@@ -32,25 +26,25 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("Server is running on port " + port);
+  console.log("Server running on port " + port);
 });
 
 app.get("/service-worker.js", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
-app.post("/payment", function (req, res) {
+app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
     currency: "usd",
   };
 
-  stripe.charges.create(body, (stripeError, stripeResponse) => {
-    if (stripeError) {
-      res.status(500).send({ error: stripeError });
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
     } else {
-      res.status(200).send({ success: stripeResponse });
+      res.status(200).send({ success: stripeRes });
     }
   });
 });

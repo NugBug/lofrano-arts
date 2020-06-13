8bc54076dff8a6e3169b8cb7161c2868265ef98b
joshtanguay <joshtanguay@gmail.com>
2020-06-12 22:34:47 -0700

    updated styling for body

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -12,7 +12,7 @@ export const GlobalStyles = createGlobalStyle`
   background: linear-gradient(to right, #eef2f3, #8e9eab); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   
     @media screen and (max-width: 800px) {
-      padding: 10px;
+      padding: 0 10px 10px;
     }
   }
 

c46419c7d578af40f50400a06833205a29f31260
joshtanguay <joshtanguay@gmail.com>
2020-06-16 23:16:24 -0700

    updated modal loading

--- client/src/components/modal/modal.component.jsx
+++ client/src/components/modal/modal.component.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import { addItem } from "../../redux/cart/cart.actions";
+import Spinner from "../../components/spinner/spinner.component";
 
 import "./modal.styles.scss";
 
 const Modal = ({ show, close, addItem, item }) => {
-  const { name, price, imageUrl, forSale } = item;
+  const { name, price, imageUrl, forSale, thumbUrl } = item;
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <div className="modal">
@@ -23,13 +26,34 @@ const Modal = ({ show, close, addItem, item }) => {
             ×
           </span>
         </div>
-        <div
-          className="modal-body"
-          onClick={close}
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}
-        ></div>
+        <div className="modal-body" onClick={close}>
+          <div
+            className="loading"
+            style={{ visibility: isLoaded ? "hidden" : "visible " }}
+          >
+            <h1>Loading...</h1>
+          </div>
+          {show ? (
+            <LazyLoadImage
+              onLoad={() => {
+                setIsLoaded(true);
+              }}
+              className="modal-image full"
+              alt={"Loading Image"}
+              style={{ opacity: isLoaded ? 1 : 0 }}
+              src={imageUrl}
+            ></LazyLoadImage>
+          ) : (
+            <LazyLoadImage
+              onLoad={() => {
+                setIsLoaded(true);
+              }}
+              className="modal-image thumb"
+              src={thumbUrl}
+            ></LazyLoadImage>
+          )}
+          {console.log(isLoaded)}
+        </div>
         <div className="modal-footer">
           {forSale ? <h3>Price: {price}</h3> : <h3>Price: --</h3>}
           {forSale ? (

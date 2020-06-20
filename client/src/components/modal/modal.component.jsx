078d800ad66e69250da8ec82d62cb62b84323313
joshtanguay <joshtanguay@gmail.com>
2020-06-19 21:35:41 -0700

    debugging modal and gallery images not loading

--- client/src/components/modal/modal.component.jsx
+++ client/src/components/modal/modal.component.jsx
@@ -12,66 +12,61 @@ const Modal = ({ show, close, addItem, item }) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <div className="modal">
-      <div
-        className="modal-wrapper"
-        style={{
-          transform: show ? "translateY(0vh)" : "translateY(-100vh)",
-          opacity: show ? "1" : "0",
-        }}
-      >
-        <div className="modal-header">
-          <h3>{name}</h3>
-          <span className="close-modal-btn" onClick={close}>
-            ×
-          </span>
+    <div
+      className="modal-wrapper"
+      style={{
+        transform: show ? "translateY(0vh)" : "translateY(-100vh)",
+        opacity: show ? "1" : "0",
+      }}
+    >
+      <div className="modal-header">
+        <h3>{name}</h3>
+        <span className="close-modal-btn" onClick={close}>
+          ×
+        </span>
+      </div>
+      <div className="modal-body" onClick={close}>
+        <div
+          className="modal-loader"
+          style={{ visibility: isLoaded ? "hidden" : "visible " }}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-        <div className="modal-body" onClick={close}>
-          <div
-            className="modal-loader"
-            style={{ visibility: isLoaded ? "hidden" : "visible " }}
-          >
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          {show ? (
-            <img
-              onLoad={() => {
-                setIsLoaded(true);
-              }}
-              className="modal-image full"
-              alt=""
-              style={{ opacity: isLoaded ? 1 : 0 }}
-              src={imageUrl}
-            ></img>
-          ) : (
-            <LazyLoadImage
-              onLoad={() => {
-                setIsLoaded(true);
-              }}
-              alt={""}
-              className="modal-image thumb"
-              src={thumbUrl}
-            ></LazyLoadImage>
-          )}
-        </div>
-        <div className="modal-footer">
-          {forSale ? <h3>Price: {price}</h3> : <h3>Price: --</h3>}
-          {forSale ? (
-            <CustomButton
-              onClick={() => addItem(item)}
-              className="btn-purchase"
-            >
-              Add To Cart
-            </CustomButton>
-          ) : (
-            <Link to="/contact">
-              <CustomButton className="btn-purchase">Inquire</CustomButton>
-            </Link>
-          )}
-        </div>
+        {show ? (
+          <img
+            onLoad={() => {
+              setIsLoaded(true);
+            }}
+            className="modal-image full"
+            alt=""
+            style={{ opacity: isLoaded ? 1 : 0 }}
+            src={imageUrl}
+          ></img>
+        ) : (
+          <LazyLoadImage
+            onLoad={() => {
+              setIsLoaded(true);
+            }}
+            alt={""}
+            className="modal-image thumb"
+            src={thumbUrl}
+          ></LazyLoadImage>
+        )}
+      </div>
+      <div className="modal-footer">
+        {forSale ? <h3>Price: {price}</h3> : <h3>Price: --</h3>}
+        {forSale ? (
+          <CustomButton onClick={() => addItem(item)} className="btn-purchase">
+            Add To Cart
+          </CustomButton>
+        ) : (
+          <Link to="/contact">
+            <CustomButton className="btn-purchase">Inquire</CustomButton>
+          </Link>
+        )}
       </div>
     </div>
   );

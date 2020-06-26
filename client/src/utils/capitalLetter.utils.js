97322dd74f8f9d19d2d87cb69e2a444d0abb89f2
joshtanguay <joshtanguay@gmail.com>
2020-06-26 16:21:35 -0700

    fixed a bug in which white space around the name string was causing the application to crash

--- client/src/utils/capitalLetter.utils.js
+++ client/src/utils/capitalLetter.utils.js
@@ -1,5 +1,6 @@
 export const capitalLetter = (str) => {
-  str = str.split(" ");
+  console.log(str);
+  str = str.trim().split(" ");
 
   for (let i = 0, x = str.length; i < x; i++) {
     str[i] = str[i][0].toUpperCase() + str[i].substr(1);

11d219f000ffe1219bd5401d21c107d95a4caafb
joshtanguay <joshtanguay@gmail.com>
2020-08-06 11:11:20 -0700

    updated footer

--- client/src/pages/terms-and-privacy/terms-and-privacy.component.jsx
+++ client/src/pages/terms-and-privacy/terms-and-privacy.component.jsx
@@ -7,9 +7,24 @@ const TermsAndPrivacy = () => {
       <div className="privacy-statement-text">
         <h2>Account and Profile Information</h2>
         <p>
-          When you create an account with Lofrano Arts and sign-up you provide
-          us with your name and email address. We do not collect any information
-          about our users from a third party.
+          When you create an account with Lofrano Arts and sign-up, you provide
+          us with your name and email address. Each account profile is assigned
+          a unique personal identification number, which helps us ensure that
+          only you can access your profile.
+          <br></br>
+          <br></br>
+          After your account has been created and your unique identification
+          number has been generate, we then send this identification number back
+          to your hard drive in the form of a cookie, which is a very small bit
+          of code. This code is uniquely yours. It is your passport to seamless
+          navigate across Lofrano Arts without having to fill out registration
+          forms with information you've already provided or to log on to Lofrano
+          Arts multiple times when navigating to and from LofranoArts.com.
+          <br></br>
+          <br></br>
+          We do not collect any information about our users from a third party.
+          We will not sell your name, email address, phone number, home address
+          or any other information you may provide to Lofrano Arts to anyone.
         </p>
         <div className="line-space"></div>
         <div className="line-space"></div>
@@ -27,12 +42,74 @@ const TermsAndPrivacy = () => {
         <hr></hr>
         <div className="line-space"></div>
         <div className="line-space"></div>
+        <h2>Security</h2>
+        <p>
+          Lofrano Arts has taken strong measures to protect the security of your
+          personal information and to ensure that your choices for its intended
+          use are honored. We take strong precautions to protect your data from
+          loss, misuse, unauthorized access or disclosure, alteration, or
+          destruction.
+          <br></br>
+          <br></br>
+          We guarantee your e-commerce transactions to be 100% safe and secure.
+          When you place orders or access your personal account information,
+          you're utilizing secure server software SSL, which encrypts your
+          personal information before it's sent over the Internet. SSL is one of
+          the safest encryption technologies available.
+          <br></br>
+          <br></br>
+          In addition, your transactions are guaranteed under the Fair Credit
+          Billing Act. This Act states that your bank cannot hold you liable for
+          more than $50.00 in fraudulent credit card charges. In the event of
+          unauthorized use of your credit card, you must notify your credit card
+          provider in accordance with its reporting rules and procedures.
+          <br></br>
+          <br></br>
+          Lofrano Arts strictly protects the security of your personal
+          information and honors your choices for its intended use. We carefully
+          protect your data from loss, misuse, unauthorized access or
+          disclosure, alteration, or destruction.
+          <br></br>
+          <br></br>
+          Your personal information is never shared outside the company without
+          your permission, except under conditions explained on this page.
+          Inside the company, data is stored in password-controlled servers with
+          limited access. Your information may be stored and processed in the
+          United States or any other country where Lofrano Arts, its
+          subsidiaries, affiliates or agents are located.
+          <br></br>
+          <br></br>
+          You also have a significant role in protecting your information. No
+          one can see or edit your personal information without knowing your
+          user name and password, so do not share these with others.
+        </p>
+        <div className="line-space"></div>
+        <div className="line-space"></div>
+        <hr></hr>
+        <div className="line-space"></div>
+        <div className="line-space"></div>
         <h2>How do we use the information we collect?</h2>
         <p>
           Your email and name are used strictly for authentication to access and
           make purchases on LofranoArts.com as well as to personalize your
           experience. We may at times send updates to the email address
           provided. You have the option to opt-out of such updates.
+          <br></br>
+          <br></br>
+          We occasionally hire other companies to provide limited services on
+          our behalf, including packaging, mailing and delivering purchases, and
+          sending postal mail. We will only provide those companies the
+          information they need to deliver the service, and they are prohibited
+          from using that information for any other purpose.
+          <br></br>
+          <br></br>
+          Lofrano Arts will disclose your personal information, without notice,
+          only if required to do so by law or in the good faith belief that such
+          action is necessary to: (a) conform to the edicts of the law or comply
+          with legal process served on Lofrano Arts or the site; (b) protect and
+          defend the rights or property of Lofrano Arts and its family of
+          Websites, and, (c) act in urgent circumstances to protect the personal
+          safety of users of Lofrano Arts, its Websites, or the public.
         </p>
         <div className="line-space"></div>
         <div className="line-space"></div>
@@ -42,7 +119,8 @@ const TermsAndPrivacy = () => {
         <h2>How do I delete my account?</h2>
         <p>
           Please contact Lofrano Arts directly via the contact link on this
-          website to request deletion of your account.
+          website to request deletion of your account. A website adniminstrator
+          will fulfill your request within 2 business days.
         </p>
         <div className="line-space"></div>
         <div className="line-space"></div>
@@ -55,6 +133,18 @@ const TermsAndPrivacy = () => {
           noted above by notifying users through e-mail. If any modification is
           unacceptable to a user, the user may request to have his/her/their
           account deleted.
+        </p>
+        <div className="line-space"></div>
+        <div className="line-space"></div>
+        <hr></hr>
+        <div className="line-space"></div>
+        <div className="line-space"></div>
+        <h2>Enforcement</h2>
+        <p>
+          If for some reason you believe Lofrano Arts has not adhered to these
+          principles, please notify via the contct link on this website, and we
+          will do our best to determine and correct the problem promptly. Be
+          certain to select Privacy Policy in the Subject line.
         </p>
       </div>
     </div>
